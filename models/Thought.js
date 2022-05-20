cbcef87aeced33a540fcef53100bde327aa7f08c
Krystopher Quintero <krystopher.q@gmail.com>
2022-05-20 00:51:12 -0700

    create thought model

---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const reactionSchema = require('./Reaction')
+const dateTime = require('../utils/dateTime')
+const thoughtSchema = new mongoose.Schema({
+  thoughtText:{
+    type:String,
+    required:true,
+    minlength:1,
+    maxlength:280
+  },
+  createdAt:{
+    type:Date,
+    default:Date.now,
+    get:(timeStamp)=>dateTime(timeStamp)
+  },
+  username:{
+    type:String,
+    required:true
+  },
+  reactions:[reactionSchema]
+})
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema)
+
+module.exports = Thought
